75511585540a8d3de179d449bce0d0086b3e588f
luchdotcom <lachezarivanov05|@gmail.com>
2024-01-06 13:13:46 +0200

    add Make files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-//
-// Created by luchdotcom on 10.08.22.
-//
-#include "iostream"
-int main(){
-    std::cout<<"hallo me...";
+#include <iostream>
 
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
